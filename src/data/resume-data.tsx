b3a4021ee922af392b3e20e604d5128d420bcbf2
Sai Vangala <saikris12@gmail.com>
2025-01-16 15:10:25 -0500

    added projects and cleaned up copy

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -14,7 +14,7 @@ export const RESUME_DATA = {
   location: "Toronto, Canada, ET",
   locationLink: "https://www.google.com/maps/place/Toronto",
   about:
-    "Senior Software Engineer and Tech Lead, Builder and Mentor with a passion for Innovation and Problem Solving",
+    "Senior Engineer and Tech Lead with a passion for innovation, mentoring teams, and solving complex problems.",
   summary:
     "As a seasoned Senior Engineer and Tech Lead, I have a proven track record of leading successful product launches with diverse, cross-functional teams. My expertise includes team leadership, fostering innovation, and delivering high-quality solutions using various programming languages and cloud technologies. I am dedicated to nurturing cohesive teams and championing a culture of excellence.",
   avatarUrl: "https://avatars.githubusercontent.com/u/19532601?v=4",
@@ -58,7 +58,7 @@ export const RESUME_DATA = {
       start: "2022",
       end: "Present",
       description:
-        "Successfully led a team in the development and nationwide launch of the Tims Mastercard, driving product development from scratch. Assembled and nurtured a high-performing team, fostering collaboration and innovation. Guided the team through the entire product lifecycle, from concept to market launch.",
+        "Successfully led a team in the development and nationwide launch of the Tims Mastercard, driving product development from scratch. Assembled and nurtured a high-performing team, promoting collaboration and innovation. Guided the team through the entire product lifecycle, from concept to market launch.",
     },
     {
       company: "Koho Financial Inc",
@@ -69,7 +69,7 @@ export const RESUME_DATA = {
       start: "2021",
       end: "2022",
       description:
-        "Part of a team that built an in-house ledger and money movement channels, ensuring robust and scalable solutions. Developed the payments infrastructure, enhancing transaction efficiency and reliability. Mentored and coached developers, fostering a collaborative and growth-oriented environment.",
+        "Part of a team that built an in-house ledger and money movement channels, ensuring robust and scalable solutions. Developed the payments infrastructure, enhancing transaction efficiency and reliability. Mentored and coached developers, cultivating a collaborative and growth-oriented environment.",
     },
     {
       company: "Interac Corp",
@@ -102,7 +102,7 @@ export const RESUME_DATA = {
       start: "2008",
       end: "2014",
       description:
-        "Developed a wide range of skills from front-end to back-end development, enabling the creation of innovative and effective solutions. uccessfully built and implemented solutions for major brands, including Singapore Airlines and USAA, demonstrating the ability to meet high standards and deliver impactful results.",
+        "Successfully built and implemented solutions for major brands, including Singapore Airlines and USAA, demonstrating the ability to meet high standards and deliver impactful results.",
     },
   ],
   skills: [
@@ -119,28 +119,33 @@ export const RESUME_DATA = {
       title: "Transaction store",
       techStack: ["DynamoDB", "Event Bridge", "Kinesis", "Lambda", "NodeJS"],
       description: "A scalable transaction datastore that delivers real-time transaction feeds to fintech mobile app users.",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://saivangala.me/",
-      },
+      
     },
     {
       title: "AI Spending Insights",
       techStack: ["DynamoDB", "Amazon Bedrock", "Lambda", "Python", "Prompt Engineering"],
       description: "An AI platform that analyzes credit card spending to provide personalized insights and recommendations.",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://saivangala.me/",
-      },
+    },
+    {
+      title: "Payment Rails Framework",
+      techStack: ["Go", "Docker", "Postgres"],
+      description: "A comprehensive framework for building diverse money movement solutions with integrated risk management rules.",
+    },
+    {
+      title: "Platform Modernization - Microservices",
+      techStack: ["Docker", "Spring Boot", "Envoy", "Oracle Cloud"],
+      description: "Transformed a legacy tech stack by containerizing a monolithic application into microservices deployed via Docker.",
+    },
+    {
+      title: "Realtime Fraud Detection",
+      techStack: ["Docker", "Spring Boot", "Kafka", "Spanner (GCP)"],
+      description: "Utilized real-time event streaming capabilities to detect fraudulent transactions instantly and alert customers promptly.",
     },
     {
       title: "saivangala.me",
-      techStack: ["Next.js", "Vercel"],
+      techStack: ["Next.js", "Vercel", "Wordpress", "Tailwind CSS"],
       description:
-        "My personal website and blog. Built with Next.js and Vercel",
-      logo: JarockiMeLogo,
+        "My personal website and blog, built with Next.js and hosted on Vercel, with the domain registered through WordPress and DNS configured for seamless integration.",
       link: {
         label: "github.com",
         href: "https://saivangala.me/",
